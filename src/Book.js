22d70895b3bedad14f269c3d7436bf867be2f912
scruffy21 <yougotmebeggingyouformercy@gmail.com>
2018-07-31 10:46:45 +0200

    accept incoming changes

--- src/Book.js
+++ src/Book.js
@@ -4,25 +4,14 @@ import React from 'react'
 
 
 class Book extends React.Component {
-<<<<<<< HEAD
-
-    // book = this.props.book
-    render() {
-        console.log(this.props)
-=======
     
     render() {
         const book = this.props.book
->>>>>>> new
         return (
             <li>
                 <div className="book">
                     <div className="book-top">
-<<<<<<< HEAD
-                        {/* <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: book.imageLinks.thumbnail }}></div> */}
-=======
                 <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
->>>>>>> new
                         <div className="book-shelf-changer">
                             <select value={book.shelf}>
                                 <option value="move" disabled>Move to...</option>
