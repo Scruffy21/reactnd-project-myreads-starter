1712f642977ca608e23a212dacccd62ff22992e9
scruffy21 <yougotmebeggingyouformercy@gmail.com>
2018-08-01 11:53:44 +0200

    fix unavailblae thumbnails error

--- src/Book.js
+++ src/Book.js
@@ -4,13 +4,15 @@ import React from 'react'
 
 class Book extends React.Component {
     
+
     render() {
         const book = this.props.book;
+        const bookCover = book.imageLinks ? `url(${book.imageLinks.thumbnail})` : null
         return (
             <li>
                 <div className="book">
                     <div className="book-top">
-                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: bookCover }}></div>
                         <div className="book-shelf-changer">
                             <select value={book.shelf || 'none'} onChange={(event) => this.props.updateBookShelf(book, event.target.value)}>
                                 <option value="move" disabled>Move to...</option>
